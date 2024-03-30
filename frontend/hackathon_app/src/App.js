d63deacc22ad419b1ab0e1e6d375b44d212c4ec1
chrisz118 <zikoc118@gmail.com>
2024-03-29 20:32:22 -0400

    Base webpage set up for input form

--- frontend/hackathon_app/src/App.js
+++ frontend/hackathon_app/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import InputForm from './InputForm/InputForm'; // Import InputForm component
 
 function App() {
   return (
@@ -18,6 +19,8 @@ function App() {
           Learn React
         </a>
       </header>
+      {/* Render the InputForm component */}
+      <InputForm />
     </div>
   );
 }
